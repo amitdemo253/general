c7643e78fce7c2ed9e39cb43a06137c6a40fac5a
amit <amitdemo253@gmail.com>
2023-01-03 13:46:32 +0530

    fifth2

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://incredible-mermaid-46e953.netlify.app/"],
+    origin: ["http://localhost:3000","https://himalyan-journal.netlify.app/"],
     credentials: true,
   })
 );
