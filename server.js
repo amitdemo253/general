ddeac418d1a35fa0c57b664150fb6fcd4676ad3f
amit <amitdemo253@gmail.com>
2023-01-03 15:16:49 +0530

    six

--- server.js
+++ server.js
@@ -9,25 +9,17 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-var allowedOrigins = ["http://localhost:3000","https://himalyan-journal.netlify.app/"];
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(
+  cors({
+    origin: ["https://himalyan-journal.netlify.app/","http://localhost:3000",],
+    credentials: true,
+  })
+);
 
 app.use(express.static(`${__dirname}/uploads`))
 // Routes Middleware
